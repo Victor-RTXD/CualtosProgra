cd232c8340a1208e2f631b67890bdeb22446f3dd
Victor Macias <victor.macias5736@alumnos.udg.mx>
2022-09-28 11:30:59 -0500

    practica 5 terminada

--- practica5/Main.java
+++ practica5/Main.java
@@ -2,16 +2,25 @@ package practica5;
 
 import java.util.Scanner;
 
+/*Implemente mediante los métodos de la clase String, una aplicación que cuente el número de vocales hay en una frase dada por el usuario.
+Para ello lo pueden implementar en la parte del main del programa. */
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String frase[] = new String[50];
-        int flag = 1;
+        String frase;
+        short contador = 0;
 
-        while(flag == 1) {
-            System.out.println("funcionando");
-            sc.nextInt(flag);
-        }
+        frase = sc.nextLine();
+        frase = frase.toLowerCase();
         
+        for (int i = 0; i < frase.length(); i++) {
+            //rase.charAt(i) == 'a' funciona como un metodo que segun el indice de la posicion del string, puede verificar si esta el caracter requerido
+            if(frase.charAt(i) == 'a' || frase.charAt(i) == 'e' || frase.charAt(i) == 'i' || frase.charAt(i) == 'o' || frase.charAt(i) == 'u') {
+                contador++;
+            }
+        }
+
+        System.out.println(contador);
     }
 }
