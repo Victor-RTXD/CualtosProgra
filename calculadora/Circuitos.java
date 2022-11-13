4170944fbf0795c809da719d459dba48054577ff
Victor Macias <victor.macias5736@alumnos.udg.mx>
2022-11-13 15:53:41 -0600

    calculadora primigod

---
+++ calculadora/Circuitos.java
@@ -0,0 +1,103 @@
+package calculadora;
+
+import java.util.Scanner;
+import java.math.*;
+
+public class Circuitos {
+    Scanner sc = new Scanner(System.in);
+    short aux;
+    double a = 0;
+    double b = 0;
+
+    public void dialogoOhm(String objetivo, String com1, String com11, String com2, String com22, String com3, String com33) {
+        System.out.println("saber" + objetivo + "con: \n");
+                System.out.println("1: " + com1+ " y " + com11);
+                System.out.println("2: " + com2 + " y " + com22);
+                System.out.println("3: " + com3+ " y " + com33);
+                aux = sc.nextShort();
+    }
+
+    public void elementosScanner(double a, double b) {
+        this.a = sc.nextDouble();
+        this.b = sc.nextDouble();
+    }
+    
+    public void leyOhm() {
+        System.out.println("escoge \n 1: Potencia \n 2: Intensidad \n 3: Tension \n 4: Resistencia");
+        
+        do {
+            aux = sc.nextShort();
+
+            switch (aux) {
+                case 1:
+                    dialogoOhm("potencia", "volts", "corriente", "resistencia", "corriente", "volts", "resistencia");
+    
+                    if (aux == 1) {
+                        System.out.println("ingresa volts y luego corriente");
+                        elementosScanner(a, b);
+                        System.out.println("\n" + (a * b));
+                    } else if (aux == 2) {
+                        System.out.println("ingresa resistencia y luego correinte");
+                        elementosScanner(a, b);
+                        System.out.println("\n" + (a * (b * b)));
+                    } else if (aux == 3) {
+                        System.out.println("ingresa volts y luego resistencia");
+                        elementosScanner(a, b);
+                        System.out.println("\n" + ((a * a) / b));
+                    }
+                break;
+                case 2:
+                    dialogoOhm("intensidad", "volts", "resistencia", "potencia", "volts", "potencia", "resistencia");
+    
+                    if (aux == 1) {
+                        System.out.println("ingresa volts y luego resistencia");
+                        elementosScanner(a, b);
+                        System.out.println("\n" + (a / b));
+                    } else if (aux == 2) {
+                        System.out.println("ingresa potencia y luego voltaje");
+                        elementosScanner(a, b);
+                        System.out.println("\n" + (a / b));
+                    } else if (aux == 3) {
+                        System.out.println("ingresa potencia y luego resistencia");
+                        elementosScanner(a, b);
+                        System.out.println("\n" + (Math.sqrt((a / b))));
+                    }
+                break;
+                case 3:
+                    dialogoOhm("tension", "potencia", "resistencia", "potencia", "corriente", "corriente", "resistencia");
+    
+                    if (aux == 1) {
+                        System.out.println("ingresa potencia y luego resistencia");
+                        elementosScanner(a, b);
+                        System.out.println("\n" + (Math.sqrt((a * b))));
+                    } else if (aux == 2) {
+                        System.out.println("ingresa potencia y luego corriente");
+                        elementosScanner(a, b);
+                        System.out.println("\n" + (a / b));
+                    } else if (aux == 3) {
+                        System.out.println("ingresa resistencia y luego corriente");
+                        elementosScanner(a, b);
+                        System.out.println("\n" + (a * b));
+                    }
+                break;
+                case 4:
+                    dialogoOhm("resistencia", "potencia", "corriente", "potencia", "volts", "volts", "corriente");
+    
+                    if (aux == 1) {
+                        System.out.println("ingresa potencia y luego corriente");
+                        elementosScanner(a, b);
+                        System.out.println("\n" + (a / (b * b)));
+                    } else if (aux == 2) {
+                        System.out.println("ingresa volts y luego potencia");
+                        elementosScanner(a, b);
+                        System.out.println("\n" + ((a * a) / b));
+                    } else if (aux == 3) {
+                        System.out.println("ingresa volts y luego corriente");
+                        elementosScanner(a, b);
+                        System.out.println("\n" + (a / b));
+                    }
+                break;
+            }
+        } while (aux != 0);
+    }
+}
---
+++ calculadora/main.java
@@ -0,0 +1,36 @@
+package calculadora;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i;
+
+        System.out.println("Selecciona la opcion deseada");
+        do {
+            System.out.println("1: Carga Eléctrica ");
+            System.out.println("2: Campo electrico");
+            System.out.println("3: Circuitos");
+            System.out.println("0: Salir");
+
+            i = sc.nextInt();
+
+            switch (i) {
+                case 1:
+                    System.out.println("funciona");
+                break;
+                case 2:
+                    System.out.println();
+                break;
+                case 3:
+                    Circuitos circuitos = new Circuitos();
+                    circuitos.leyOhm();
+                break;
+                default:
+                    System.out.println();
+                break;
+            }
+        } while (i != 0);
+    }
+}
