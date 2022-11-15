479fb4aab8f10229b490176428bcf0b7f313797a
Victor Macias <victor.macias5736@alumnos.udg.mx>
2022-11-14 20:38:45 -0600

    acabando ley coulomb

--- calculadora/CargaElectrica.java
+++ calculadora/CargaElectrica.java
@@ -13,49 +13,41 @@ public class CargaElectrica {
     double f1;
     double f2;
 
+    public void procedimiento(float q1, float q2, float r, double exponente) {
+        System.out.println("ingresa q1");
+        this.q1 = sc.nextFloat();
+        System.out.println("ingresa el exponente: ");
+        exponente1 = sc.nextShort();
+        exponente1 = Math.pow(10, -exponente1);
+        System.out.println("ingresa q2");
+        this.q2 = sc.nextFloat();
+        System.out.println("ingresa distancia: ");
+        this.r = sc.nextFloat();
+    }
     public void leyCoulomb() {
         System.out.println("favor de no poner numeros negativos");
 
-        System.out.println("ingresa q1");
-        q1 = sc.nextFloat();
-        System.out.println("ingresa el exponente: ");
-        exponente1 = sc.nextShort();
-        exponente1 = Math.pow(10, -exponente1);
-        System.out.println("ingresa q2");
-        q2 = sc.nextFloat();
-        System.out.println("ingresa distancia: ");
-        r = sc.nextFloat();
-        f1 = (k * ((q1 * exponente1)  * (q2 * exponente1)))/(r*r);
+        do {
+            procedimiento(q1, q2, r, exponente1);
+            f1 = (k * ((q1 * exponente1)  * (q2 * exponente1)))/(r*r);
+            System.out.println(f1);
 
-        System.out.println(f1);
+            System.out.println("quieres ingresar otra fuerza?  (oprimir 1)");
 
-        System.out.println("quieres saber conocer otra fuerza?  Y/n");
+            if (i == 1) {
+                procedimiento(q1, q2, r, exponente1);
+                f2 = (k * ((q1 * exponente1)  * (q2 * exponente1)))/(r*r);
+                System.out.println(f2);
+                
+                System.out.println("Magnitud resultante: " + Math.sqrt((f1 * f1) + (f2 * f2)));
+                k = Math.atan(f1 / f2);
+                System.out.println("grados con f1 sobre f2: " + Math.toDegrees(k));
+                k = Math.atan(f2 / f1);
+                System.out.println("grados con f2 sobre f1: " + Math.toDegrees(k));
 
-        System.out.println("ingresa q1");
-        q1 = sc.nextFloat();
-        System.out.println("ingresa el exponente: ");
-        exponente1 = sc.nextShort();
-        exponente1 = Math.pow(10, -exponente1);
-        System.out.println("ingresa q2");
-        q2 = sc.nextFloat();
-        System.out.println("ingresa distancia: ");
-        r = sc.nextFloat();
-        f2 = (k * ((q1 * exponente1)  * (q2 * exponente1)))/(r*r);
-
-        System.out.println(f2);
-
-        System.out.println("quieres saber magnitud y direccion resultante?  Y/n");
-
-        i = sc.nextShort();
-
-        if (i == 1) {
-            System.out.println("Magnitud resultante: " + Math.sqrt((f1 * f1) + (f2 * f2)));
-            k = Math.atan(f1 / f2);
-            System.out.println("grados con f1 sobre f2: " + Math.toDegrees(k));
-            k = Math.atan(f2 / f1);
-            System.out.println("grados con f2 sobre f1: " + Math.toDegrees(k));
-        } 
-
-
+                System.out.println("oprime 0 para salir o 1 para intentar de nuevo");
+                i = sc.nextShort();
+            }
+        } while (i != 0);
     }
 }
