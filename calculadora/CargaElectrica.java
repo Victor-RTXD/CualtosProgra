2dd7a2659434977e9576822ea56b9fe58e48f0c8
Victor Macias <victor.macias5736@alumnos.udg.mx>
2022-11-14 20:43:06 -0600

    detalles coulomb

--- calculadora/CargaElectrica.java
+++ calculadora/CargaElectrica.java
@@ -16,11 +16,11 @@ public class CargaElectrica {
     public void procedimiento(float q1, float q2, float r, double exponente) {
         System.out.println("ingresa q1");
         this.q1 = sc.nextFloat();
+        System.out.println("ingresa q2");
+        this.q2 = sc.nextFloat();
         System.out.println("ingresa el exponente: ");
         exponente1 = sc.nextShort();
         exponente1 = Math.pow(10, -exponente1);
-        System.out.println("ingresa q2");
-        this.q2 = sc.nextFloat();
         System.out.println("ingresa distancia: ");
         this.r = sc.nextFloat();
     }
@@ -30,14 +30,15 @@ public class CargaElectrica {
         do {
             procedimiento(q1, q2, r, exponente1);
             f1 = (k * ((q1 * exponente1)  * (q2 * exponente1)))/(r*r);
-            System.out.println(f1);
+            System.out.println("f1: " + f1);
 
-            System.out.println("quieres ingresar otra fuerza?  (oprimir 1)");
+            System.out.println("oprime 1 para ingresar otra fuerza, otro numero para acabar ");
+            i = sc.nextShort();
 
             if (i == 1) {
                 procedimiento(q1, q2, r, exponente1);
                 f2 = (k * ((q1 * exponente1)  * (q2 * exponente1)))/(r*r);
-                System.out.println(f2);
+                System.out.println("f2: " + f2);
                 
                 System.out.println("Magnitud resultante: " + Math.sqrt((f1 * f1) + (f2 * f2)));
                 k = Math.atan(f1 / f2);
