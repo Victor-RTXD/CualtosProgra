a82a92683fc26515b4db8ea8e4e3e4d5afa0bf0a
Victor <89050826+VictorRTXD@users.noreply.github.com>
2023-01-23 19:05:15 +0000

    cola

--- act1/Clase.java
+++ act1/Clase.java
@@ -1,10 +1,18 @@
 package act1;
+import java.util.Scanner;
 
 public class Clase {
+    Scanner sc = new Scanner(System.in);
     int codigo;
     String materia;
     int calificacion;
+    int front = -1;
+    int rear = -1;
+    int item;
+    int size = 5;
+    Integer queue[] = new Integer[size];
 
+    //metodos
     public Clase(int calificacion, int codgio, String materia) {
         this.calificacion = calificacion;
         this.materia = materia;
--- act1/Main.java
+++ act1/Main.java
@@ -2,19 +2,21 @@ package act1;
 
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
         int tCodigo, tCalificacion;
         String tMateria;
-
         Scanner sc = new Scanner(System.in);
-        Clase cualtos[]= new Clase[10];
+        Clase cualtos[]= new Clase[2];
 
         for (int i = 0; i < cualtos.length; i++) {
             System.out.println("ingresa la calificacion: ");
             tCalificacion = sc.nextInt();
+
             System.out.println("ingresa el codigo: ");
             tCodigo = sc.nextInt();
+
             System.out.println("ingresa la materia: ");
             tMateria = sc.next();
 
@@ -26,3 +28,6 @@ public class Main {
         }
     }
 }
+
+
+
