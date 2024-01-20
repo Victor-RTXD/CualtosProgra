d6b3f42c057ed29d22613a9af9bdc03442a80514
VictorRTXD <victor.macias5736@alumnos.udg.mx>
2024-01-20 09:37:56 -0600

    iniciando sexto

--- Sergio/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$db_username = "root";
-$db_password = "Victor4061";
-$db_name = "lab3";
-
-$link = mysqli_connect($host,$db_username,$db_password,$db_name) or
-	die ("Ha fallado la conexion".mysqli_connect_error());
-?>
--- Sergio/insercionesVictor.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO suppliers (id_people, companyname) values(6, 'provedor A');
-insert into buys(suppliers, quantity, payment_method, totalprice) values(6,5,1,4000);
