76a925ddabf041b9b8089285d99aabf3535d8f5d
VictorRTXD <victor.macias5736@alumnos.udg.mx>
2025-03-22 23:50:57 -0600

    add(agregando proyecto react entorno sustentable)

---
+++ nature/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Home from './components/Home'
+import Stats from './components/Stats'
+import News from './components/News'
+import Practices from './components/Practices'
+import Onu from './components/Onu'
+
+function App() {
+  return (
+    <>
+      <Home />
+      <Onu />
+      <Stats />
+      <News />
+      <Practices />
+    </>
+  )
+}
+
+export default App
---
+++ nature/src/components/Home/index.jsx
@@ -0,0 +1,4 @@
+export default function Home() {
+    return <div className="bg-blue-500 text-white p-4 rounded-lg">Tailwind funciona</div>;
+}
+  
